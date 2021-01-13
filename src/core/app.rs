4e66c753ee4ed0b671107c36217b91e6cc921733
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-12 18:18:34 -0800

    created app struct fields

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,4 +1,18 @@
-pub struct App
+use crate::input::Input;
+
+use super::Time;
+
+/// handle to the application running this `Sketch`.
+/// this is how `Canvas`es are created or input and
+/// time is accessed.
+///
+/// internally, the `App` struct is hollow; it holds
+/// only references to its composing components(time,
+/// input, etc.) which `framework::run` feeds to it
+/// whenever needed. this doesn't change much on the
+/// user end, other than `App` not being `'static`
+pub struct App<'a>
 {
-    
+    pub(super) input: &'a Input,
+    pub(super) time: &'a Time,
 }
