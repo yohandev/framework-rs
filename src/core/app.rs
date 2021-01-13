893a02566dd4e7adb1bb681e6e359a00805fada4
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-12 19:29:30 -0800

    moved input, time, etc. into app

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,43 +1,133 @@
-use crate::input::{ Input, Mouse, Keys };
-use crate::draw::CanvasId;
+use std::collections::HashMap;
+
+use winit::event_loop::{ ControlFlow, EventLoopWindowTarget as WindowTarget };
+use winit::window::WindowId;
+use winit::event::Event;
+
+use crate::input::{ Input, Mouse, Keys, ProcessedEvent };
+use crate::draw::{ CanvasId, Window };
+use crate::core::{ Time, Sketch };
 use crate::math::Extent2;
-use crate::core::Time;
 
 /// handle to the application running this `Sketch`.
 /// this is how `Canvas`es are created or input and
 /// time is accessed.
-///
-/// internally, the `App` struct is hollow; it holds
-/// only references to its composing components(time,
-/// input, etc.) which `framework::run` feeds to it
-/// whenever needed. this doesn't change much on the
-/// user end, other than `App` not being `'static`
-pub struct App<'a>
+pub struct App
 {
-    pub(super) input: &'a Input,
-    pub(super) time: &'a Time,
+    /// input state
+    pub(super) input: Input,
+    /// time state
+    pub(super) time: Time,
 
-    pub(super) requests: Vec<AppRequest>
+    /// windows currently open
+    pub(super) windows: HashMap::<WindowId, Window>,
+    /// window creation requests
+    pub(super) requests: Vec<(String, Extent2<usize>)>
 }
 
-/// queued instructions for an `App` that are then
-/// polled by `framework::run`
-pub(super) enum AppRequest
+impl App
 {
-    /// create a new window/canvas
-    CreateCanvas(String, Extent2<usize>),
-    // close a canvas
-    CloseCanvas(CanvasId),
-}
+    /// create a new app
+    pub(super) fn new() -> Self
+    {
+        Self
+        {
+            input: Input::new(),
+            time: Time::new(),
+            windows: HashMap::new(),
+            requests: Vec::new(),
+        }
+    }
 
-impl<'a> App<'a>
-{
+    /// process an incoming winit event and interact with
+    /// `Sketch`, inner `Time`, `Input`, and windows accordingly.
+    pub(super) fn process_event(
+        &mut self,
+        sketch: &mut impl Sketch,
+        event: Event<()>,
+        window_target: &WindowTarget<()>,
+        control_flow: &mut ControlFlow
+    )
+    {
+        match self.input.process(event)
+        {
+            // notify `Pixels` of the window resize
+            ProcessedEvent::WindowResized(id, (w, h)) =>
+            {
+                if let Some(window) = self.windows.get_mut(&id)
+                {
+                    window.pixels.resize(w, h);
+                }
+            }
+            // close window and quit if needed
+            ProcessedEvent::WindowClose(id) =>
+            {
+                // stop keeping track of and drop window
+                self.windows.remove(&id);
+
+                // quit?
+                if self.windows.is_empty()
+                {
+                    return *control_flow = ControlFlow::Exit;
+                }
+            }
+            // render to the given window
+            ProcessedEvent::ShouldRender(id) =>
+            {
+                // get the frame
+                if let Some(window) = self.windows.get_mut(&id)
+                {
+                    // update buffer
+                    sketch.draw(&mut window.get_frame());
+
+                    // render
+                    if window.pixels.render().is_err()
+                    {
+                        return *control_flow = ControlFlow::Exit;
+                    }
+                }
+            }
+            // update the Sketch state and request new events
+            ProcessedEvent::ShouldUpdate =>
+            {
+                // update time
+                self.time.update();
+
+                // update state
+                sketch.update(self);
+
+                // process requests
+                self.process_requests(window_target);
+                
+                // request a redraw on each window
+                for (_, window) in self.windows.iter_mut()
+                {
+                    window.winit.request_redraw();
+                }
+            }
+            ProcessedEvent::None => { }
+        }
+    }
+
+    /// process window creation requests. this should be
+    /// called right after the `App` has been mutuably passed
+    /// to the `Sketch`
+    pub(super) fn process_requests(&mut self, target: &WindowTarget<()>)
+    {
+        while let Some((title, size)) = self.requests.pop()
+        {
+            let window = Window::new(target, title, size);
+
+            self.windows.insert(window.winit.id(), window);
+        }
+    }
+
     /// get the current `Time` information for this
     /// frame
     #[inline]
     pub fn time(&self) -> &Time
     {
-        self.time
+        &self.time
     }
 
     /// get the current state of the keyboard for this
@@ -60,13 +150,13 @@ impl<'a> App<'a>
     /// draw to
     pub fn create_canvas(&mut self, title: impl Into<String>, size: impl Into<Extent2<usize>>)
     {
-        self.requests.push(AppRequest::CreateCanvas(title.into(), size.into()));
+        self.requests.push((title.into(), size.into()));
     }
 
     /// destroys a `Canvas` given its ID if it exists, or
     /// does nothing if it doesn't
     pub fn destroy_canvas(&mut self, id: CanvasId)
     {
-        self.requests.push(AppRequest::CloseCanvas(id));
+        self.windows.remove(&id.0);
     }
 }
--- src/core/run.rs
+++ src/core/run.rs
@@ -1,13 +1,7 @@
-use std::collections::HashMap;
+use winit::event_loop::EventLoop;
 
-use winit::event_loop::{ ControlFlow, EventLoop, EventLoopWindowTarget as WindowTarget };
-use winit::window::WindowId;
+use crate::core::{ Sketch, App };
 
-use crate::input::{ Input, ProcessedEvent };
-use crate::core::{ Sketch, Time, App };
-use crate::draw::Window;
-
-use super::app::AppRequest;
 
 /// run the sketch, hyjacking the main thread until the
 /// window is closed
@@ -16,107 +10,15 @@ pub fn run<T: Sketch>()
     // create event loop
     let events = EventLoop::new();
 
-    // windows
-    let mut windows = HashMap::<WindowId, Window>::new();
-
-    // inputs
-    let mut input = Input::new();
-    let mut time = Time::new();
+    // app & sketch
+    let mut app = App::new();
+    let mut sketch = T::setup(&mut app);
     
-    // callbacks to avoid code duplication
-    let app = ||
+    // create initial windows
+    app.process_requests(&events);
+
+    events.run(move |event, window_target, control_flow|
     {
-        App
-        {
-            input: &input,
-            time: &time,
-            requests: Vec::with_capacity(0),
-        }
-    };
-    let mut process_requests = |target: &WindowTarget<()>, requests: Vec<AppRequest>|
-    {
-        for request in requests
-        {
-            match request
-            {
-                AppRequest::CreateCanvas(title, size) =>
-                {
-                    let window = Window::new(target, title, size);
-
-                    windows.insert(window.winit.id(), window);
-                }
-                AppRequest::CloseCanvas(id) =>
-                {
-                    windows.remove(&id.0);
-                }
-            }
-        }
-    };
-    
-    // create the `Sketch`
-    let mut sketch =
-    {
-        // setup with an `App`
-        let mut app = app();
-        let sketch = T::setup(&mut app);
-
-        process_requests(&events, app.requests);
-
-        sketch
-    };
-
-    events.run(move |event, _, control_flow|
-    {
-        match input.process(event)
-        {
-            // notify `Pixels` of the window resize
-            ProcessedEvent::WindowResized(id, (w, h)) =>
-            {
-                windows[&id].pixels.resize(w, h);
-            }
-            // close window and quit if needed
-            ProcessedEvent::WindowClose(id) =>
-            {
-                // stop keeping track of and drop window
-                windows.remove(&id);
-
-                // quit?
-                if windows.is_empty()
-                {
-                    return *control_flow = ControlFlow::Exit;
-                }
-            }
-            // render to the given window
-            ProcessedEvent::ShouldRender(id) =>
-            {
-                // get the frame
-                let window = windows[&id];
-                
-                // update buffer
-                sketch.draw(&mut window.get_frame());
-
-                // render
-                if window.pixels.render().is_err()
-                {
-                    return *control_flow = ControlFlow::Exit;
-                }
-            }
-            // update the Sketch state and request new events
-            ProcessedEvent::ShouldUpdate =>
-            {
-                // update time
-                time.update();
-
-                // update state
-                sketch.update(&mut app());
-                
-                // request a redraw on each window
-                for (_, window) in windows.iter_mut()
-                {
-                    window.winit.request_redraw();
-                }
-            }
-            ProcessedEvent::None => { }
-        }
+        app.process_event(&mut sketch, event, window_target, control_flow);
     });
 }
