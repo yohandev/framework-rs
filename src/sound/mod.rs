c1d996cdf67d42d5879cb24abcfd282b928d4290
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-14 22:11:56 -0800

    deleted old code and added dependencies

--- src/sound/mod.rs
+++ src/sound/mod.rs
@@ -1,10 +1,3 @@
-mod speakers;
-mod silence;
-mod sample;
 mod track;
-mod file;
 
-pub use self::speakers::{ Audio, AudioErr };
-pub use self::file::{ SoundFile, SoundFileError };
-pub use self::sample::{ Sample, SampleType };
-pub use self::track::{ Track, RawTrack };
+pub use self::track::Track;
--- src/sound/track.rs
+++ src/sound/track.rs
@@ -1,85 +1,25 @@
-use crate::sound::sample::{ Sample, SampleType };
+use std::sync::Arc;
 
-/// an iterator that specifically yields samples to be
-/// written to an audio output stream
-pub trait Track: Send + Sync + 'static
+use rodio::{ Sink, Sample };
+
+/// represents an audio track, with playback, volume,
+/// and other audio controls. 
+pub struct Track<S: Sample>
 {
-    /// format of the sample returned by this iterator
-    type Format: Sample;
+    sink: Sink,
 
-    /// get the next sample (and?) increment self for
-    /// the next iteration
-    fn next_sample(&mut self) -> Option<Self::Format>;
-
-    /// called once whenever this track is passed to a
-    /// `Speakers` instance
-    fn tune(&mut self, channels: usize, sample_rate: usize);
+    /// this audio file's interleaved samples
+    samples: Arc<[S]>,
+    /// number of channels in this file
+    channels: usize,
+    /// next index
+    index: usize,
 }
 
-/// abstraction over `Track` to permit dynamic
-/// types. it works directly with bytes and
-/// has access to the entire stream segment, rather
-/// than working like an iterator
-pub trait RawTrack: Send + Sync + 'static
+impl<S: Sample> Track<S>
 {
-    /// write `self`'s next sample directly to the stream.
-    /// keep on writing until either `self as Track`
-    /// is exhausted or the end of `stream` is reached.
-    ///
-    /// assumes `stream % sizeof(format) == 0`
-    ///
-    /// returns `true` if `self as Track` is done
-    fn write_samples(&mut self, stream: &mut [u8], format: SampleType) -> bool;
-
-    /// called once whenever this track is passed to a
-    /// `Speakers` instance
-    fn tune(&mut self, channels: usize, sample_rate: usize);
-}
-
-impl<S: Sample, T: Track<Format = S>> RawTrack for T
-{
-    fn write_samples(&mut self, mut stream: &mut [u8], format: SampleType) -> bool
+    pub fn open()
     {
-        macro_rules! write_samples
-        {
-            ($ty:ident, $siz:literal) =>
-            {
-                loop
-                {
-                    // done with stream segment but not track
-                    if stream.is_empty() { break false; }
-
-                    // not done with track
-                    if let Some(s) = self.next_sample()
-                    {
-                        // write next sample
-                        stream[0..$siz].copy_from_slice(&s.$ty().to_ne_bytes());
-
-                        // next iteration
-                        stream = &mut stream[$siz..];
-                    }
-                    // done with track
-                    else
-                    {
-                        break true;
-                    }
-                }
-            }
-        }
-
-        use SampleType::*;
-
-        // actually write
-        match format
-        {
-            F32 => write_samples!(to_f32, 4),
-            I16 => write_samples!(to_i16, 2),
-            U16 => write_samples!(to_u16, 2),
-        }
-    }
-
-    fn tune(&mut self, channels: usize, sample_rate: usize)
-    {
-        Track::tune(self, channels, sample_rate);
+        
     }
 }
