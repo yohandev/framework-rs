c811e9831d02184e066cc7bbc10f98a0c8898597
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-15 15:24:01 -0800

    contiguous pixel buf [mut]

--- src/draw/bitmap/buf.rs
+++ src/draw/bitmap/buf.rs
@@ -5,6 +5,9 @@ use crate::math::Rgba;
 /// while the entire buffer doesn't have to be in
 /// contiguous memory, the only requirement is that
 /// each row of pixels *is* in contiguous memory.
+///
+/// note: implementors of `PixelBuf` should *never*
+/// grow or shrink
 pub unsafe trait PixelBuf
 {
     /// get the raw pixels at the given column
@@ -76,7 +79,45 @@ pub unsafe trait PixelBufMut: PixelBuf
 /// implenting type has a reference to the entire buffer
 /// of pixels in contiguous memory. that is, row 1 is
 /// right before row 2 which is right before row 3, etc.
-pub trait ContiguousPixelBuf: PixelBuf
+///
+/// [PixelBuf]: self::PixelBuf
+pub unsafe trait ContiguousPixelBuf: PixelBuf
 {
+    /// get the pixels in `u8` representation, aligned in
+    /// memory row-by-row
+    fn raw_pixels<'a>(&self) -> &'a [u8];
 
+    /// get the pixels aligned in memory row-by-row
+    fn pixels<'a>(&self) -> &'a [Rgba<u8>]
+    {
+        use std::slice::from_raw_parts as slice;
+
+        let buf = self.raw_pixels();
+        unsafe
+        {
+            slice(buf.as_ptr() as *const Rgba<u8>, buf.len() >> 2)
+        }
+    }
+}
+
+/// mutable version of [ContiguousPixelBuf]
+///
+/// [ContiguousPixelBuf]: self::ContiguousPixelBuf
+pub unsafe trait ContiguousPixelBufMut: ContiguousPixelBuf
+{
+    /// get the pixels in `u8` representation, aligned in
+    /// memory row-by-row
+    fn raw_pixels_mut<'a>(&mut self) -> &'a mut [u8];
+
+    /// get the pixels aligned in memory row-by-row
+    fn pixels_mut<'a>(&mut self) -> &'a mut [Rgba<u8>]
+    {
+        use std::slice::from_raw_parts_mut as slice;
+
+        let buf = self.raw_pixels_mut();
+        unsafe
+        {
+            slice(buf.as_ptr() as *mut Rgba<u8>, buf.len() >> 2)
+        }
+    }
 }
